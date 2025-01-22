4e8b9ef3136e3b55ec6bce339a3a09caea9a8380
Sparsha Ramakrishna <sparshar3@gmail.com>
2025-01-22 15:31:19 -0800

    intialize scores and dice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const diceEl = document.querySelector('.dice');
+const score0 = document.getElementById('score--0');
+const score1 = document.querySelector('#score--1');
+
+score0.textContent = '0';
+score1.textContent = '0';
+
+diceEl.classList.add('hidden');
